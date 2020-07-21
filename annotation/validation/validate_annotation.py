02edce3362735b16fc04718d65a95327756cee9e
saggu <amandeep.s.saggu@gmail.com>
2020-07-20 17:12:42 -0700

    more validation, convert to library

--- annotation/validation/validate_annotation.py
+++ annotation/validation/validate_annotation.py
@@ -1,20 +1,107 @@
 import pandas as pd
 import json
+from xlsxwriter import utility
+
+ROLE_ROW = 2
+TYPE_ROW = 3
 
 
 class VaidateAnnotation(object):
     def __init__(self):
         self.error_report = []
+        self.valid_roles = {
+            'main subject': 1,
+            'time': 1,
+            'location': 1,
+            'variable': 1,
+            'qualifier': 1,
+            'unit': 1
+        }
+        self.valid_types = {
+            'main subject': ['string', 'entity'],
+            'time': ['year', 'month', 'day'],
+            'location': ['admin1', 'admin2', 'admin3', 'longitude', 'latitude', 'country', 'city'],
+            'variable': ['number'],
+            'qualifier': ['string'],
+            'unit': ['string']
+        }
 
-    def validate(self, file_path=None, df=None):
-        if file_path is None and df is None:
-            raise Exception('Please provide a file path or a dataframe to validate')
-        if file_path:
-            df = pd.read_excel(file_path).fillna('')
+    def validate(self, file_path):
+        df = pd.read_excel(file_path, header=None).fillna('')
 
         valid_column_one = self.validate_annotation_column_one(df)
         if not valid_column_one:
             raise Exception(json.dumps(self.error_report))
+
+        valid_roles = self.validate_roles_types(df)
+        if not valid_roles:
+            raise Exception(json.dumps(self.error_report))
+
+        valid_role_and_type = self.validate_roles_types(df)
+        if not valid_role_and_type:
+            raise Exception(json.dumps(self.error_report))
+
+    def validate_roles_types(self, df):
+        roles = list(df.iloc[1])
+        types = list(df.iloc[2])
+        invalid_roles = []
+
+        for i in range(1, len(roles)):
+            if roles[i].strip() not in self.valid_roles and roles[i] != '':
+                invalid_roles.append(utility.xl_col_to_name(i))
+
+        if invalid_roles:
+            self.error_report.append(
+                self.error_row('Roles in following column(s) are invalid: {}'.format(','.join(invalid_roles)),
+                               ROLE_ROW, ','.join(invalid_roles),
+                               'Valid roles are one of the following: {}'.format(','.join(self.valid_roles))))
+            return False
+
+        prev_role = None
+        valid_types = True
+        for i in range(1, len(types)):
+            r = roles[i].strip()
+            t = types[i].strip()
+
+            if r != '':
+                prev_role = r
+            if r == '':
+                r = prev_role
+            if t == '' and r != '':
+                self.error_report.append(self.error_row(
+                    'Missing type for role: {}'.format(r),
+                    TYPE_ROW,
+                    utility.xl_col_to_name(i),
+                    'Please specify a valid type. Valid type for role: {} is one of the following: [{}]'.format(r,
+                                                                                                                ','.join(
+                                                                                                                    self.valid_types[
+                                                                                                                        r]))
+                ))
+                valid_types = False
+
+            elif t != '':
+                if r == 'time':
+                    if t not in self.valid_types[r] and not t.startswith('%'):
+                        self.error_report.append(self.error_row(
+                            'Invalid type: {}, for the role: {}'.format(t, r),
+                            TYPE_ROW,
+                            utility.xl_col_to_name(i),
+                            'Valid type for role: {}, is either one of [{}], OR a python date format '
+                            'regex(https://docs.python.org/3.7/library/datetime.html#strftime-and-strptime-behavior)'
+                                .format(r, ','.join(self.valid_types[r]))
+                        ))
+                        valid_types = False
+                else:
+                    if t not in self.valid_types[r]:
+                        self.error_report.append(self.error_row(
+                            'Invalid type: {}, for the role: {}'.format(t, r),
+                            TYPE_ROW,
+                            utility.xl_col_to_name(i),
+                            'Valid type for role: {}, is [{}]'.format(r, ','.join(self.valid_types[r]))
+                        ))
+                        valid_types = False
+
+        return valid_types
 
     def validate_annotation_column_one(self, df):
         valid_first_column = True
@@ -59,8 +146,3 @@ class VaidateAnnotation(object):
             'Column': column,
             'Description': description
         }
-
-
-va = VaidateAnnotation()
-df = pd.read_excel('sample_annotation.xlsx', header=None)
-va.validate_annotation_column_one(df)
